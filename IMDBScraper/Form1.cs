7bb81c23b2a7fb0e2d035fc8892428d4bbf66962
joker99 <joker99@gmail.com>
2014-06-28 11:47:47 +0300

    added support for coming sonn page

--- IMDBScraper/Form1.cs
+++ IMDBScraper/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace IMDBScraper
 {
@@ -21,7 +23,13 @@ namespace IMDBScraper
         private void btnScrape_Click(object sender, EventArgs e)
         {
             btnScrape.Enabled = false;
-            var imdbIds = txtIO.Text.Split(new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+            var imdbIds = txtIO.Text.Split(new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            // the input can be a link to coming soon page (http://www.imdb.com/movies-coming-soon/) or list of imdb id's
+            if (imdbIds.Count == 1 && imdbIds[0].IndexOf("imdb.com") != 0)
+            {
+                imdbIds = ParseComingSoonPage(imdbIds[0]).ToList();
+            }
+            // parse list of id's
             var result = string.Join(Environment.NewLine, imdbIds.AsParallel().Select((id, i) =>
             {
                 var imdbEntry = new ImdbEntry(id);
@@ -33,6 +41,31 @@ namespace IMDBScraper
             }).OrderBy(kvp => kvp.Key).Select(kvp=>kvp.Value));            
             txtIO.Text = result.ToString();
             btnScrape.Enabled = true;
+        }
+
+        private IEnumerable<string> ParseComingSoonPage(string url)
+        {
+            var downloader = (HttpWebRequest)HttpWebRequest.Create(url);
+            downloader.Headers[HttpRequestHeader.AcceptLanguage] = "en-US,en;q=0.5";
+            downloader.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate";
+            downloader.Headers[HttpRequestHeader.Pragma] = "no-cache";
+            downloader.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)";
+            downloader.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            var document = new HtmlAgilityPack.HtmlDocument();
+            try
+            {
+                using (StreamReader sr = new StreamReader(downloader.GetResponse().GetResponseStream()))
+                {
+                    document.Load(sr);
+                }
+            }
+            catch (WebException ex)
+            {
+
+            }
+
+            var urlNodes = document.DocumentNode.SelectNodes("//a[@itemprop='url']");
+            return urlNodes.Select(node=> node.Attributes["href"].Value.Split(new string[] {"/"},StringSplitOptions.RemoveEmptyEntries)[1]).Where(id=>id.StartsWith("tt"));
         }
 
         private void txtIO_KeyDown(object sender, KeyEventArgs e)
