a8170755f93dc88f7715f87aff00308c57b8d312
joker99 <joker99@gmail.com>
2014-12-19 14:49:07 +0200

    added retry 3 time on failure

--- IMDBScraper/ImdbEntry.cs
+++ IMDBScraper/ImdbEntry.cs
@@ -18,38 +18,43 @@ namespace IMDBScraper
         public IEnumerable<string> Genres { get; private set; }
         public ImdbEntry(string id)
         {
-            Genres = new string[0];
-            ImdbId = id;
-            var downloader = (HttpWebRequest)HttpWebRequest.Create("http://www.imdb.com/title/" + ImdbId + "/");
-            downloader.Headers[HttpRequestHeader.AcceptLanguage] = "en-US,en;q=0.5";
-            downloader.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate";
-            downloader.Headers[HttpRequestHeader.Pragma] = "no-cache";
-            downloader.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)";
-            downloader.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            var document = new HtmlDocument();
-            try
+            // try 3 times, we are talking about internet....
+            for (int i = 0; i < 3; i++)
             {
-                using (StreamReader sr = new StreamReader(downloader.GetResponse().GetResponseStream()))
+                try
                 {
-                    document.Load(sr);
+                    Genres = new string[0];
+                    ImdbId = id;
+                    var downloader = (HttpWebRequest)HttpWebRequest.Create("http://www.imdb.com/title/" + ImdbId + "/");
+                    downloader.Headers[HttpRequestHeader.AcceptLanguage] = "en-US,en;q=0.5";
+                    downloader.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate";
+                    downloader.Headers[HttpRequestHeader.Pragma] = "no-cache";
+                    downloader.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)";
+                    downloader.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                    var document = new HtmlDocument();
+                    using (StreamReader sr = new StreamReader(downloader.GetResponse().GetResponseStream()))
+                    {
+                        document.Load(sr);
+                    }
+                    Name = System.Net.WebUtility.HtmlDecode(document.DocumentNode.SelectSingleNode("//span[@itemprop='name']").InnerText.Trim());
+                    var yearNode = document.DocumentNode.SelectSingleNode("//h1[@class='header']").Elements("span").FirstOrDefault(e => e.Element("a") != null);
+                    if (yearNode != null)
+                        Year = int.Parse(yearNode.Element("a").InnerText);
+                    var ratingNode = document.DocumentNode.SelectSingleNode("//span[@itemprop='ratingValue']");
+                    if (ratingNode != null && ratingNode.InnerText.Trim() != "-")
+                    {
+                        Rating = double.Parse(ratingNode.InnerText.Trim());
+                        var ratingCount = document.DocumentNode.SelectSingleNode("//span[@itemprop='ratingCount']");
+                        if (ratingCount != null)
+                            RatingCount = int.Parse(ratingCount.InnerText.Trim(), System.Globalization.NumberStyles.AllowThousands);
+                        Genres = document.DocumentNode.SelectSingleNode("//div[@class='infobar']").Elements("a").Select(n => System.Net.WebUtility.HtmlDecode(n.InnerText).Trim());
+                    }
+                    break;
                 }
-            }
-            catch (WebException ex)
-            { 
-            
-            }
-            Name = System.Net.WebUtility.HtmlDecode(document.DocumentNode.SelectSingleNode("//span[@itemprop='name']").InnerText.Trim());
-            var yearNode = document.DocumentNode.SelectSingleNode("//h1[@class='header']").Elements("span").FirstOrDefault(e =>e.Element("a") != null);
-            if(yearNode != null)
-                Year = int.Parse(yearNode.Element("a").InnerText);
-            var ratingNode = document.DocumentNode.SelectSingleNode("//span[@itemprop='ratingValue']");
-            if (ratingNode != null && ratingNode.InnerText.Trim() != "-")
-            {
-                Rating = double.Parse(ratingNode.InnerText.Trim());
-                var ratingCount = document.DocumentNode.SelectSingleNode("//span[@itemprop='ratingCount']");
-                if(ratingCount != null)
-                    RatingCount = int.Parse(ratingCount.InnerText.Trim(), System.Globalization.NumberStyles.AllowThousands);
-                Genres = document.DocumentNode.SelectSingleNode("//div[@class='infobar']").Elements("a").Select(n => System.Net.WebUtility.HtmlDecode(n.InnerText).Trim());
+                catch (Exception ex)
+                { 
+
+                }
             }
         }
     }
